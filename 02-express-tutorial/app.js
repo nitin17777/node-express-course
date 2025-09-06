41d9f0d1b941dcded4c9b4b5db40c1ba61d9f8a9
Nitin <puneranitin@gmail.com>
2025-09-06 16:28:46 +0530

    Learnt some more topics in nodejs and starting express js

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,24 @@
-console.log('Express Tutorial')
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+app.use(express.static('./public'))
+
+app.get('/', (req,res)=>
+{
+    res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
+})
+
+
+app.all('*', (req,res)=>
+{
+    req.status(404).send('resource not found')
+
+})
+
+app.listen(5000,()=>
+{
+    console.log('Server is listening on port 5000....');
+})
